b4ca8a11386037c9e8675b5762d87c601ee62e20
Tyler Blanco <42285917+Tblanco17@users.noreply.github.com>
2021-03-01 12:24:19 -0500

    Update ConfigureRemotingForAnsible.ps1

--- examples/scripts/ConfigureRemotingForAnsible.ps1
+++ examples/scripts/ConfigureRemotingForAnsible.ps1
@@ -120,19 +120,19 @@ Trap
 $ErrorActionPreference = "Stop"
 
 # Get the ID and security principal of the current user account
-$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
-$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+#$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+#$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
 
 # Get the security principal for the Administrator role
-$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+#$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
 
 # Check to see if we are currently running "as Administrator"
-if (-Not $myWindowsPrincipal.IsInRole($adminRole))
-{
-    Write-Output "ERROR: You need elevated Administrator privileges in order to run this script."
-    Write-Output "       Start Windows PowerShell by using the Run as Administrator option."
-    Exit 2
-}
+#if (-Not $myWindowsPrincipal.IsInRole($adminRole))
+#{
+   # Write-Output "ERROR: You need elevated Administrator privileges in order to run this script."
+  #  Write-Output "       Start Windows PowerShell by using the Run as Administrator option."
+ #   Exit 2
+#}
 
 $EventSource = $MyInvocation.MyCommand.Name
 If (-Not $EventSource)
