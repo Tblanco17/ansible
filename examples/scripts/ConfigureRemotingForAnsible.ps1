5b089037a5ce061809ba0cd59f0024ec3ef42215
Tyler Blanco <42285917+Tblanco17@users.noreply.github.com>
2021-03-08 12:20:56 -0500

    Update ConfigureRemotingForAnsible.ps1

--- examples/scripts/ConfigureRemotingForAnsible.ps1
+++ examples/scripts/ConfigureRemotingForAnsible.ps1
@@ -112,39 +112,7 @@ Function New-LegacySelfSignedCert
 
 
 
-# Setup error handling.
-Trap
-{
-    $_
-    Exit 1
-}
-$ErrorActionPreference = "Stop"
 
-# Get the ID and security principal of the current user account
-$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
-$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
-
-# Get the security principal for the Administrator role
-$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
-
-# Check to see if we are currently running "as Administrator"
-if (-Not $myWindowsPrincipal.IsInRole($adminRole))
-{
-    Write-Output "ERROR: You need elevated Administrator privileges in order to run this script."
-    Write-Output "       Start Windows PowerShell by using the Run as Administrator option."
-    Exit 2
-}
-
-$EventSource = $MyInvocation.MyCommand.Name
-If (-Not $EventSource)
-{
-    $EventSource = "Powershell CLI"
-}
-
-If ([System.Diagnostics.EventLog]::Exists('Application') -eq $False -or [System.Diagnostics.EventLog]::SourceExists($EventSource) -eq $False)
-{
-    New-EventLog -LogName Application -Source $EventSource
-}
 
 # Detect PowerShell version.
 If ($PSVersionTable.PSVersion.Major -lt 3)
